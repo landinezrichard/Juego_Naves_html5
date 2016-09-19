d4e41ed7efda38e7c368a6e80d4c27c2bfdae842
Ricardo <landinezrichard@gmail.com>
2016-09-19 16:53:37 -0500

    Dibujando Circulos

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -6,44 +6,62 @@ function init(){
 	var ctx = canvas.getContext('2d');
 
 	/*
-	Cambia el alpha global del color
+	- Cambia el alpha global del color
+		(recibe valores entre el 0 al 1)
+		
+		ctx.globalAlpha = 0.5;
 
-	recibe valores entre el 0 al 1
+	- Cambia el color de relleno
+
+		ctx.fillStyle = 'red';
+
+	- Traza y dibuja un rectangulo relleno
+		ctx.fillRect(coordX,coordY,ancho,alto);
+	
+		ctx.fillRect(20,10,50,60);
+
+	- Cambia el color del borde
+
+		ctx.strokeStyle = "blue";
+
+	- Modifica el ancho de línea (default = 1)
+
+		ctx.lineWidth = 10;
+
+	- Traza y dibuja un rectangulo sin relleno
+	
+		ctx.strokeRect(90,90,50,50);
+
+	- Borra, en forma de rectangulo:
+
+		ctx.clearRect(x,y,ancho.alto);
+
+	- Genera el trazo de un arco (se usa para circulos)
+
+		ctx.arc(x,y,radio,anguloInicial,anguloFinal,antiReloj);
+
+		Los angulos se dan en radianes.
+
+		El valor "antiReloj" es un booleano(default = false).
+
+		Si lo dejamos sin: "ctx.stroke()" ó "ctx.fill()", no dibuja nada
+
+	- Dibuja el trazo de la figura:
+
+		ctx.stroke();
+
+	- Rellena el trazo:
+
+		ctx.fill();
 	*/
 
-	ctx.globalAlpha = 0.5;
+	ctx.arc(20,20,10,0,convertirRadianes(360));
 
-	/*
-	Cambia el color de relleno
-	*/
-	ctx.fillStyle = 'red';
+	ctx.arc(50,50,10,0,convertirRadianes(180));
 
-	/*
-	Dibuja un rectangulo relleno
-	ctx.fillRect(coordX,coordY,ancho,alto);
-	*/
+	ctx.arc(80,80,10,0,convertirRadianes(180),true);
 
-	ctx.fillRect(20,10,50,60);
-
-	/*
-	Cambia el color del borde
-	*/
-
-	ctx.strokeStyle = "blue";
-
-	/*
-	Modifica el ancho de línea (default = 1)
-	*/
-
-	ctx.lineWidth = 10;
-
-	/*
-	Dibuja en rectangulo sin relleno
-	*/
-
-	ctx.strokeRect(90,90,50,50);
-
-	erase(canvas,ctx);
+	ctx.stroke();
 }
 
 
@@ -55,4 +73,8 @@ function erase(canvas, ctx){
 	En este caso borra todo el lienzo o canvas.
 	*/
 	ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function convertirRadianes(grados){
+	return (grados * Math.PI) / 180;
 }
