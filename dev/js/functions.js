9bef9388bc3c5d59042513f172ed7e393e424f94
Ricardo <landinezrichard@gmail.com>
2016-09-19 16:07:27 -0500

    Entendiendo el contexto del canvas - colores

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -6,9 +6,26 @@ function init(){
 	var ctx = canvas.getContext('2d');
 
 	/*
-	Dibuja un rectangulo
+	Cambia el color de relleno
+	*/
+	ctx.fillStyle = 'red';
+
+	/*
+	Dibuja un rectangulo relleno
 	ctx.fillRect(coordX,coordY,ancho,alto);
 	*/
 
 	ctx.fillRect(20,10,50,60);
+
+	/*
+	Cambia el color del borde
+	*/
+
+	ctx.strokeStyle = "blue";
+
+	/*
+	Dibuja en rectangulo sin relleno
+	*/
+
+	ctx.strokeRect(90,90,50,50);
 }
