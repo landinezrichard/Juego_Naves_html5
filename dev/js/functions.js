714606b11c346f64e0d3fc9b08b911ea9ea3e0d2
Ricardo <landinezrichard@gmail.com>
2016-09-21 14:03:24 -0500

    Algoritmo de Colisiones

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -182,6 +182,26 @@ function actualizaEnemigos(){
 	};
 }
 
+function colisiones(a,b){
+	var hit = false;
+	if(b.x + b.width >= a.x && b.x < a.x+ a.width){
+		if(b.y + b.height >= a.y && b.y < a.y + a.height){
+			hit = true;
+		}
+	}
+	if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+		if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+			hit = true;
+		}
+	}
+	if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+		if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+			hit = true;
+		}
+	}
+	return hit;
+}
+
 function frameLoop(){
 	moverNave();
 	moverProyectiles();
